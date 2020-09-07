83cc560fb83bdca2e716b5f68d99a2713297afcb
Brianna Bullock <brianna.bullock16@gmail.com>
2020-09-07 02:52:30 -0500

    fixed duration missing data error

--- models/workout.js
+++ models/workout.js
@@ -7,46 +7,10 @@ const workoutSchema = new Schema({
       type: Date,
       default: Date.now
     },
-    exercises: [
-        {
-            type: {
-                type: String,
-                trim: true,
-                required: "Enter a type of workout!"
-            }
-        },
-        {
-            name: {
-                type: String,
-                trim: true,
-                required: "Enter a name for your workout!"
-            }
-        },
-        {
-            duration: {
-                type: Number,
-                required: "Enter the number of minutes that you worked out!"
-            }
-        },
-        {
-            weight: {
-                type: Number,
-                required: "Enter your total weight!"
-            }
-        },
-        {
-            reps: {
-                type: Number,
-                required: "Enter the number of reps that you completed!"
-            }
-        },
-        {
-            sets: {
-                type: Number,
-                required: "Enter the number of sets that you completed!"
-            }
-        }
-    ]
+    exercises: {
+        type: Array,
+        default: []
+    }
 
 });
   
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,13 @@
 const db = require("../models");
 
+
 module.exports = function(app) {
     // gets the last workout
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+        .sort({_id:-1}).limit(1)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
         })
         .catch(err => {
             res.status(400).json(err);
@@ -17,6 +19,34 @@ module.exports = function(app) {
         db.Workout.create(body)
         .then(newWorkout => {
             res.json(newWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    });
+
+    // adds exercise
+    app.put("/api/workouts/:id", (req, res) => {
+        const _id = req.params.id;
+        const body = req.body
+        db.Workout.findByIdAndUpdate(
+            { _id },
+            { $push: 
+                { exercises: body } }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    // gets workout range
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
         })
         .catch(err => {
             res.status(400).json(err);
