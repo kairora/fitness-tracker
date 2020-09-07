5ae0c9606d4bf8b4501b89e42c31eb8e91ed20e1
Brianna Bullock <brianna.bullock16@gmail.com>
2020-09-06 23:06:42 -0500

    html routes added

--- server.js
+++ server.js
@@ -6,19 +6,21 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8080
 
 const app = express();
-app.use(morgan("combined"))
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
